d14aa33dfeac86b997465d876d0092242f54c680
GrishakV <vovan095@gmail.com>
2020-11-11 04:52:18 +0300

    Примеры DONE

---
+++ module_1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- config: utf-8 -*-
+
+if __name__ == '__main__':
+    with open('text.txt', 'r')as f:
+        text = f.read()
+
+        text = text.replace("!", ".")
+        text = text.replace("?", ".")
+
+        while ".." in text:
+            text.replace("..", ".")
+
+        sentences = text.split(".")
+
+        for sentence in sentences:
+            if "," in sentence:
+                print(sentence)
