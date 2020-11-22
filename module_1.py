3b7cdff9feac32f0d5ea07678c224d45401ae1b6
GrishakV <vovan095@gmail.com>
2020-11-22 19:30:37 +0300

    1 task

--- module_1.py
+++ module_1.py
@@ -15,4 +15,8 @@ if __name__ == '__main__':
 
         for sentence in sentences:
             if "," in sentence:
-                print(sentence)
+                with open('text.txt', 'r')as s:
+                    f_text = s.read()
+                    if sentence in f_text:
+                        print(f'{sentence}{f_text[f_text.rfind(sentence)]}')
+        print(f'{f_text.rfind(sentence)}')
